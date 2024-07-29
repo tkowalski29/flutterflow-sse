74ab473383a39f1bb58e666d652a56b5afc6456c
Tomasz Kowalski <tkowalski@iseno.net>
2024-07-29 16:11:03 +0200

    test

---
+++ sse.ts
@@ -0,0 +1,69 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import bodyParser from 'body-parser';
+
+dotenv.config();
+const port = 3000;
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+});
+
+app.post('/api/mobile', (req, res) => {
+    console.log("Nowe połączenie SSE");
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.flushHeaders();
+
+    const sendEvent = (data: any) => {
+        try {
+            res.write(`data: ${JSON.stringify(data)}\n\n`);
+        } catch (err) {
+            console.error('Error send data:', err);
+            clearInterval(intervalId);
+            res.end();
+        }
+    };
+
+    let counter = 0;
+    const exampleData = () => ({
+        content: `Mock message ${counter++}`,
+    });
+
+    const intervalId = setInterval(() => {
+        if (counter >= 10) {
+            clearInterval(intervalId);
+            console.log('Stop 10 message');
+            res.end();
+            return;
+        }
+        const data = exampleData();
+        console.log('Send data:', data);
+        sendEvent(data);
+    }, 500);
+
+    req.on('close', () => {
+        clearInterval(intervalId);
+        console.log('Close connection');
+        res.end();
+    });
+
+    req.on('error', (err) => {
+        clearInterval(intervalId);
+        console.error('Error connection:', err);
+        res.end();
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});
