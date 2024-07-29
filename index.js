63522ba8b7b459584cf40894efb03b2b9055d901
Tomasz Kowalski <tkowalski@iseno.net>
2024-07-29 17:02:33 +0200

    test

--- index.js
+++ index.js
@@ -20,53 +20,53 @@ app.use((req, res, next) => {
     next();
 });
 
-app.post('/sse', (req, res) => {
-    console.log("Nowe połączenie SSE");
-    res.setHeader('Content-Type', 'text/event-stream');
-    res.setHeader('Cache-Control', 'no-cache');
-    res.setHeader('Connection', 'keep-alive');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.flushHeaders();
+// app.post('/sse', (req, res) => {
+//     console.log("Nowe połączenie SSE");
+//     res.setHeader('Content-Type', 'text/event-stream');
+//     res.setHeader('Cache-Control', 'no-cache');
+//     res.setHeader('Connection', 'keep-alive');
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.flushHeaders();
 
-    const sendEvent = (data) => {
-        try {
-            res.write(`data: ${JSON.stringify(data)}\n\n`);
-        } catch (err) {
-            console.error('Error send data:', err);
-            clearInterval(intervalId);
-            res.end();
-        }
-    };
+//     const sendEvent = (data) => {
+//         try {
+//             res.write(`data: ${JSON.stringify(data)}\n\n`);
+//         } catch (err) {
+//             console.error('Error send data:', err);
+//             clearInterval(intervalId);
+//             res.end();
+//         }
+//     };
 
-    let counter = 0;
-    const exampleData = () => ({
-        content: `Mock message ${counter++}`,
-    });
+//     let counter = 0;
+//     const exampleData = () => ({
+//         content: `Mock message ${counter++}`,
+//     });
 
-    const intervalId = setInterval(() => {
-        if (counter >= 10) {
-            clearInterval(intervalId);
-            console.log('Stop 10 message');
-            res.end();
-            return;
-        }
-        const data = exampleData();
-        console.log('Send data:', data);
-        sendEvent(data);
-    }, 500);
+//     const intervalId = setInterval(() => {
+//         if (counter >= 10) {
+//             clearInterval(intervalId);
+//             console.log('Stop 10 message');
+//             res.end();
+//             return;
+//         }
+//         const data = exampleData();
+//         console.log('Send data:', data);
+//         sendEvent(data);
+//     }, 500);
 
-    req.on('close', () => {
-        clearInterval(intervalId);
-        console.log('Close connection');
-        res.end();
-    });
+//     req.on('close', () => {
+//         clearInterval(intervalId);
+//         console.log('Close connection');
+//         res.end();
+//     });
 
-    req.on('error', (err) => {
-        clearInterval(intervalId);
-        console.error('Error connection:', err);
-        res.end();
-    });
-});
+//     req.on('error', (err) => {
+//         clearInterval(intervalId);
+//         console.error('Error connection:', err);
+//         res.end();
+//     });
+// });
 
 const port = process.env.PORT || 8080
 
